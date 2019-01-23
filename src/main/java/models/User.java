85870321e40b1400c88ae17622cbfaedacbd25ae
Kevin Hassenfratz <hassenfratz.k@husky.neu.edu>
2019-01-22 23:04:09 -0500

    Added constructor to User class to enable test data in UserService

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -7,7 +7,16 @@ public class User {
 	private String firstName;
 	private String lastName;
 	
- 	public String getFirstName() {
+ 	public User(Integer id, String username, String password, String firstName, String lastName) {
+ 		this.id = id;
+ 		this.username = username;
+ 		this.password = password;
+ 		this.firstName = firstName;
+ 		this.lastName = lastName;
+		
+	}
+
+	public String getFirstName() {
 		return firstName;
 	}
  	
